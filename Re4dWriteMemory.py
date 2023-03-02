e25fc561352ec9bfeb203806729e8df23d0300ee
iiwwnnaa <>
2023-03-03 00:02:52 +0900

    Added OpenProcess, ReadProcessMemory

--- Re4dWriteMemory.py
+++ Re4dWriteMemory.py
@@ -10,11 +10,11 @@ class Re4dWriteMemory:
         self.MAX_PATH = 200
         
     def EnumProcesses(self):
-        length = 100 # length for create pid list
+        length = 100 # length for assign pid list
         while 1:
-            Pids = (ctypes.wintypes.DWORD*length)() # create empty pid list (DWORD 4byte * length)
+            Pids = (ctypes.wintypes.DWORD*length)() # assign empty pid list (DWORD 4byte * length)
             PidsSize = ctypes.sizeof(Pids) # calculate byte size of pid list
-            RByteSize = ctypes.wintypes.DWORD() # create DWORD type space for returned byte size from Pids
+            RByteSize = ctypes.wintypes.DWORD() # assign DWORD type space for returned byte size from Pids
             if Windll.Psapi.EnumProcesses(ctypes.byref(Pids), PidsSize, ctypes.byref(RByteSize)):
                 if RByteSize.value < PidsSize: # if current pid list size is fine
                     return Pids, RByteSize.value # return Pids and PidsSize
@@ -29,7 +29,7 @@ class Re4dWriteMemory:
             Pid = Pids[i] # Read Pid list in order
             hProcess = Windll.kernel32.OpenProcess(self.PROCESS_ALL_ACCESS, False, Pid) # Get Process Handle
             if hProcess:
-                ImageFileName = (ctypes.c_char*self.MAX_PATH)() # create char type space for returned file name
+                ImageFileName = (ctypes.c_char*self.MAX_PATH)() # assign char type space for returned file name
                 if Windll.psapi.GetProcessImageFileNameA(hProcess, ImageFileName, self.MAX_PATH) > 0: #If function succeeds
                     fName = os.path.basename(ImageFileName.value).decode() #Extract basename from full path 
                     if fName == pName: #If this process is matched to pName
@@ -37,13 +37,28 @@ class Re4dWriteMemory:
                         return Pid
             self.CloseHandle(hProcess)
         return 0 # FAILED to find process
-        
+
+    def OpenProcess(self, dwProcessId):
+        bInheritHandle = False
+        hProcess = Windll.kernel32.OpenProcess(self.PROCESS_ALL_ACCESS, bInheritHandle, dwProcessId)
+        print(hProcess)
+        if hProcess:
+            return hProcess
+
+    def GetPointer(self, hProcess, lpBaseAddress, offsets):
+        pass
 
     def ReadProcessMemory(self, hProcess, lpBaseAddress):
         try:
-            Windll.kernel32.ReadProcessMemory(self.PROCESS_ALL_ACCESS)
-        except:
-            pass
+            ReadBuffer = ctypes.c_uint() # assign unsigned int buffer for reading content from lpBaseAddress
+            lpBuffer = ctypes.byref(ReadBuffer) # assign pointer and point ReadBuffer
+            nSize = ctypes.sizeof(ctypes.c_uint()) # the number of bytes to be read from the process
+            #lpNumberOfBytesRead = ctypes.c_ulong(0) # A pointer to a variable that receives the number of bytes transferred into the lpBuffer.
+            Windll.kernel32.ReadProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, 0) # leave lpNumberOfBytesRead to NULL
+            return ReadBuffer
+        except (BufferError, ValueError, TypeError) as e: #Handlig Errors
+            self.CloseHandle(hProcess)
+            return f"{str(e)} raised on {hProcess} handle. Err Code : {self.GetLastError()}"
     
     def CloseHandle(self, hProcess):
         Windll.kernel32.CloseHandle(hProcess)
@@ -59,4 +74,6 @@ class Re4dWriteMemory:
     
 if __name__ == '__main__':
     rwm = Re4dWriteMemory()
-    print(rwm.GetPidByName("notepad.exe"))
+    pid = rwm.GetPidByName("Tutorial-i386.exe")
+    hProcess = rwm.OpenProcess(pid)
+    print(rwm.ReadProcessMemory(hProcess, 0x00400000).value)
