25bb99b7fe1751bdc6e0d2c2d33c26bff04d8532
iiwwnnaa <>
2023-03-04 01:42:05 +0900

    added main.py

--- Re4dWriteMemory.py
+++ Re4dWriteMemory.py
@@ -3,7 +3,7 @@ import ctypes.wintypes
 
 Windll = ctypes.windll
 
-class Re4dWriteMemory:
+class RWM:
     def __init__(self):
         #Define All access permisson
         self.PROCESS_ALL_ACCESS = 0x001F0FFF
@@ -96,14 +96,3 @@ class Re4dWriteMemory:
     
     def ClearLastError(self): 
         Windll.kernel32.SetLastError(0)
-
-if __name__ == '__main__':
-    rwm = Re4dWriteMemory()
-    pid = rwm.GetPidByName("Tutorial-i386.exe")
-    hProcess = rwm.OpenProcess(pid)
-    baseaddr = 0x00400000+0x0017F1E0
-    #print(hex(baseaddr))
-    print(hex(rwm.GetPointer(hProcess, baseaddr, offsets=[0x3DC, 0x540, 0x1C, 0xA0, 0xC, 0x218, 0x4B0])))
-    #print(rwm.ReadProcessMemory(hProcess, baseaddr).value)
-    #rwm.WriteProcessMemory(hProcess,baseaddr, 0x64)
-    #print(rwm.ReadProcessMemory(hProcess, baseaddr).value)
---
+++ main.py
@@ -0,0 +1,12 @@
+import Re4dWriteMemory
+
+if __name__ == '__main__':
+    rwm = Re4dWriteMemory.RWM()
+    pid = rwm.GetPidByName("Tutorial-i386.exe")
+    hProcess = rwm.OpenProcess(pid)
+    baseaddr = 0x00400000+0x0017F1E0
+    #print(hex(baseaddr))
+    print(hex(rwm.GetPointer(hProcess, baseaddr, offsets=[0x3DC, 0x540, 0x1C, 0xA0, 0xC, 0x218, 0x4B0])))
+    #print(rwm.ReadProcessMemory(hProcess, baseaddr).value)
+    #rwm.WriteProcessMemory(hProcess,baseaddr, 0x64)
+    #print(rwm.ReadProcessMemory(hProcess, baseaddr).value)
